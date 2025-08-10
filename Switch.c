cf4967e44958b01de5f70af100a20ce55e4a95ae
MONIK SHELCY J <93462980+Monik-jk@users.noreply.github.com>
2025-08-10 14:08:00 +0530

    Update Switch.c

--- Switch.c
+++ Switch.c
@@ -3,15 +3,18 @@
 int main()
 {
   char ch;
-  printf ("Enter a alphabet");
-  scanf (%c, &ch);
-switch(ch)
-  case 1 "a" :
-  case 2 "e" :
-  case 3 "i" :
-  case 4 "o" :
-  case 5 "u" :
-      printf( "%c is a vowal",&ch);
+  printf ("Enter a alphabet : ");
+  scanf (" %c", &ch);
+switch(ch){
+  case 'a' :
+  case 'e' :
+  case 'i' :
+  case 'o' :
+  case 'u' :
+      printf( "%c is a vowal",ch);
+      break;
 default:
-      printf("%c is a consonent");
+      printf("%c is a consonent",ch);
+}
+    return 0;
 }
