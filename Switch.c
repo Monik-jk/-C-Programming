1f6ea9d21ea64535f798cb41b442c5c5e0c8bea7
MONIK SHELCY J <93462980+Monik-jk@users.noreply.github.com>
2025-08-10 13:55:55 +0530

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  char ch;
+  printf ("Enter a alphabet");
+  scanf (%c, &ch);
+switch(ch)
+  case 1 "a" :
+  case 2 "e" :
+  case 3 "i" :
+  case 4 "o" :
+  case 5 "u" :
+      printf( "%c is a vowal",&ch);
+default:
+      printf("%c is a consonent");
+}
